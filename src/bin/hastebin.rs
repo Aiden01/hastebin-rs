7f5580957e53419f2a1231bb55a25fa57b5f3363
WebD <34129960+Webd01@users.noreply.github.com>
2018-10-28 20:57:40 -0400

    feat: Add --open option (#1)
    This option makes that the user's browser is open when the file has been uploaded to Hastebin.

--- src/bin/hastebin.rs
+++ src/bin/hastebin.rs
@@ -1,12 +1,9 @@
-
 extern crate clap;
 extern crate hastebin;
 
-use clap::{
-    App,
-    Arg,
-    ArgMatches
-};
+use clap::{App, Arg, ArgMatches};
+use hastebin::Os;
+use std::process::Command;
 
 /**
  * Build the cli
@@ -15,22 +12,76 @@ fn build_cli() -> App<'static, 'static> {
     App::new("hastebin")
         .version("1.0.0")
         .author("WebD")
-        .arg(Arg::with_name("FILE")
-            .help("The file that you want to send to hastebin")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("CHARS")
-            .help("Maximun characters to send")
-            .short("c")
-            .long("chars")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("FILE")
+                .help("The file that you want to send to hastebin")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("CHARS")
+                .help("Maximun characters to send")
+                .short("c")
+                .long("chars")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("OPEN")
+                .help("Opens your web browser when it's uploaded.")
+                .short("o")
+                .long("open"),
+        )
+}
+
+/**
+ * Returns the operating system of the user
+*/
+fn get_os() -> Os {
+    if cfg!(target_os = "linux") {
+        Os::Linux
+    } else if cfg!(target_os = "macos") {
+        Os::Macos
+    } else if cfg!(target_os = "windows") {
+        Os::Window
+    } else {
+        Os::Unknow
+    }
+}
+
+/**
+ * Opens the user's browser when the upload is finished
+*/
+fn open_browser(url: String) {
+    let win = &format!("/C {}", url);
+
+    let (cmd, arg): (&str, &str) = match get_os() {
+        Os::Window => ("cmd", win),
+        Os::Macos => ("open", &url),
+        Os::Linux => ("xdg-open", &url),
+        Os::Unknow => panic!("Unknow OS, cannot open your browser"),
+    };
+
+    Command::new(cmd)
+        .arg(arg)
+        .output()
+        .expect("Cannot open your browser");
 }
 
 fn main() {
     // Get the arguments
     let args: ArgMatches = build_cli().get_matches();
+
     if let Some(file_name) = args.value_of("FILE") {
-       println!("Uploading file...");
-       hastebin::upload_buffer(file_name, args.value_of("CHARS"));
-    } 
+        println!("Uploading file...");
+
+        match hastebin::upload_buffer(file_name, args.value_of("CHARS")) {
+            Ok(url) => {
+                println!("File uploaded successfully: {}", url);
+                if args.is_present("OPEN") {
+                    open_browser(url);
+                }
+            }
+            Err(e) => println!("An error occurred: {}", e),
+        };
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,36 @@
-extern crate reqwest;
 extern crate json;
+extern crate reqwest;
 mod file;
 use self::file::HastebinFile;
+use reqwest::Client;
 use std::fs;
 use std::io::Read;
-use reqwest::Client;
-
 
 /**
  * Upload the buffer to hastebin
  */
-pub fn upload_buffer(file_name: &str, max_chars: Option<&str>) {
-    // Checking for any errors while reading the file
-    match read_file(file_name) {
-        Ok(hastebin_file) => {
-            // Checking if the file is too big
-            if hastebin_file.is_too_big() {
-                println!("Error: File is too big, 50MB max.");
-                std::process::exit(0);
-            } else {
-                // upload the file to hastebin
-                let client = Client::new();
-                let mut response = client.post("https://hastebin.com/documents")
-                    .body(hastebin_file.get_buffer())
-                    .send()
-                    .expect("Cannot send the response");
-                // parse the response body
-                match json::parse(&response.text().unwrap()) {
-                    Ok(res_json) => println!("File uploaded successfully: https://hastebin.com/{}", res_json["key"].to_string()),
-                    Err(e) => println!("Failed to parse the body: {}", e)
-                }
-                
-            }
-        },
-        Err(e) => println!("An error occurred while reading the file: {}", e)
+pub fn upload_buffer<'a>(
+    file_name: &'a str,
+    max_chars: Option<&'a str>,
+) -> std::io::Result<String> {
+    let hastebin_file = read_file(file_name)?;
+
+    if hastebin_file.is_too_big() {
+        println!("Error: File is too big, 50MB max.");
+        std::process::exit(0);
+    } else {
+        // upload the file to hastebin
+        let client = Client::new();
+        let mut response = client
+            .post("https://hastebin.com/documents")
+            .body(hastebin_file.get_buffer())
+            .send()
+            .expect("Cannot send the response");
+
+        // parse the response body
+        let res_json = json::parse(&response.text().unwrap()).unwrap();
+        let url = format!("https://hastebin.com/{}", res_json["key"].to_string());
+        Ok(url)
     }
 }
 
@@ -48,4 +45,11 @@ fn read_file(path: &str) -> std::io::Result<HastebinFile> {
     file.read_to_string(&mut buffer)?;
     // return HastebinFile object with the informations of the file
     Ok(HastebinFile::new(buffer, String::from(path)))
+}
+
+pub enum Os {
+    Window,
+    Linux,
+    Macos,
+    Unknow,
 }
